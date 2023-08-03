5a719e264d1afa65cb526a15ed6d44373f9f4515
NikitaKx9 <nikitakx9@gmail.com>
2023-08-03 11:30:39 +0300

    holeGame changed

--- src/js/holeGame.js
+++ src/js/holeGame.js
@@ -1,25 +1,18 @@
 const holes = [...document.querySelectorAll('.hole')];
 
 function game() {
-  let playing = true,
-    activeHole = 1;
+  let activeHole = 1;
 
-  const stop = () => playing = true,
-    getHole = index => document.getElementById(`hole${index}`),
-    deactivateHole = index =>
-      getHole( index ).className = 'hole',
-    activateHole = index =>
-      getHole( index ).className = 'hole hole_has-goblin',
-    next = () => setTimeout(() => {
-      if ( !playing ) {
-        return;
-      }
-      deactivateHole( activeHole );
-      activeHole = Math.floor( 1+ Math.random() * 16 );
-      activateHole( activeHole );
-      next();
-    }, 800 );
+  const getHole = (index) => document.getElementById(`hole${index}`);
+  const deactivateHole = (index) => getHole(index).className = 'hole';
+  const activateHole = (index) => getHole(index).className = 'hole hole_has-goblin';
+  const next = () => setTimeout(() => {
+    deactivateHole(activeHole);
+    activeHole = Math.floor(1 + Math.random() * 16);
+    activateHole(activeHole);
+    next();
+  }, 800);
 
   next();
 }
-game()
+game();
