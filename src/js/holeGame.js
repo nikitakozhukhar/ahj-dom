2821caff784510f51c00a0de47b1ea4a3be5fc1d
NikitaKx9 <nikitakx9@gmail.com>
2023-08-03 11:40:47 +0300

    holeGame changed

--- src/js/holeGame.js
+++ src/js/holeGame.js
@@ -1,11 +1,13 @@
 const holes = [...document.querySelectorAll('.hole')];
-
+holes.forEach((hole) => hole.addEventListener('click', () => {
+  console.log('click');
+}));
 function game() {
   let activeHole = 1;
 
   const getHole = (index) => document.getElementById(`hole${index}`);
-  const deactivateHole = (index) => getHole(index).className = 'hole';
-  const activateHole = (index) => getHole(index).className = 'hole hole_has-goblin';
+  const deactivateHole = (index) => { getHole(index).className = 'hole'; };
+  const activateHole = (index) => { getHole(index).className = 'hole hole_has-goblin'; };
   const next = () => setTimeout(() => {
     deactivateHole(activeHole);
     activeHole = Math.floor(1 + Math.random() * 16);
